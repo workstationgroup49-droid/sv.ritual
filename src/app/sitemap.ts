9ccdf68d3f4af5a910f82fa474fb61c59c09b141
unknown <SQL>
2026-06-03 14:53:45 +0300

    init: запуск проекту

---
+++ src/app/sitemap.ts
@@ -0,0 +1,8 @@
+export default function sitemap() {
+  return [
+    { url: 'https://sv-ritual.pp.ua',            lastModified: new Date() },
+    { url: 'https://sv-ritual.pp.ua/catalog',    lastModified: new Date() },
+    { url: 'https://sv-ritual.pp.ua/granite',    lastModified: new Date() },
+    { url: 'https://sv-ritual.pp.ua/exhibition', lastModified: new Date() },
+  ]
+}
